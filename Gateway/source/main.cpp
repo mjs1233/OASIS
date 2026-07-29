75284e7ce865fac121a8616ac705265b9e5a7c90
mojongsab <apcbchet@gmail.com>
2026-07-29 16:11:36 +0900

    server basic example code

--- Gateway/source/main.cpp
+++ Gateway/source/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+
+#include "httplib.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    httplib::Server server;
+    server.Get("/",
+        [](const httplib::Request& req, httplib::Response& res) {
+
+        res.status = 200;
+        res.set_content("I'm here\n", "text/plain");
+
+    });
+    server.Post("/signal", [](const httplib::Request& req,httplib::Response& res) {
+
+    });
+
+    server.listen("0.0.0.0", 8080);
+
     return 0;
 }
