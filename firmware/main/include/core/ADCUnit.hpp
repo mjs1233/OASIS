21d33082c01fca5abe4591b835f9a9c6c2a3ce44
mojongsab <apcbchet@gmail.com>
2026-08-03 11:06:26 +0900

    add ADCUnit v2

--- firmware/main/include/core/ADCUnit.hpp
+++ firmware/main/include/core/ADCUnit.hpp
@@ -6,20 +6,70 @@
 #define FIRMWARE_ADCUNIT_HPP
 #include <cinttypes>
 #include <esp_adc/adc_oneshot.h>
+#include <esp_adc/adc_continuous.h>
+#include <array>
+#include <freertos/FreeRTOS.h>
+#include "freertos/semphr.h"
+
 namespace oasis {
+    namespace __details {
+        constexpr uint32_t default_adc_unit_num = 0;
+
+        class OneshotADC {
+
+        };
+
+        class ContinuousADC {
+        public:
+            ContinuousADC() {
+
+            }
+        };
+    }
+
+
     class ADCUnit {
     public:
-        ADCUnit(int32_t adc_num);
+        ADCUnit() {}
         ~ADCUnit();
-        void add_channel(uint32_t channel);
-        int raw_read(uint32_t channel);
-        //unit: mv
-        int read(uint32_t channel);
+        int32_t add_oneshot_gpio(int gpio) {
+            if (m_oneshot_gpio_count == 4) {
+                return -1;
+            }
+            m_oneshot_gpio[m_oneshot_gpio_count] = gpio;
+            m_oneshot_gpio_count++;
+            return m_oneshot_gpio_count - 1;
+        }
+
+        void set_continuous_gpio(int gpio) {
+            m_continuous_gpio = gpio;
+        }
+
+        bool read_oneshot(std::array<uint8_t, 4>& buffer, int wait) {
+            if (xSemaphoreTake(m_adc_mutex,pdMS_TO_TICKS(wait)) == pdTRUE) {
+
+
+            }
+            else {
+                return false;
+            }
+        };
+
+        bool read_continuous(uint8_t* data,size_t length, uint32_t read_time) {
+
+        };
 
     private:
-        int32_t adc_num;
-        adc_oneshot_unit_handle_t adc_handle;
-        adc_cali_handle_t cali_handle;
+        std::array<int, 4> m_oneshot_gpio{};
+        int32_t m_oneshot_gpio_count = 0;
+
+        int m_continuous_gpio{};
+
+        SemaphoreHandle_t m_adc_mutex;
+
+
+        adc_oneshot_unit_handle_t m_adc_handle;
+        adc_cali_handle_t m_cali_handle;
 
     };
 }
